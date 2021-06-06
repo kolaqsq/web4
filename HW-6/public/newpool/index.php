dde2fef3f26aaa3403c67c682b3e6953ed22d21c
kolaqsq <kolaqsq1707@gmail.com>
2021-06-06 18:22:34 +0300

    started docker integration

---
+++ HW-6/public/newpool/index.php
@@ -0,0 +1,4 @@
+<?php
+echo '<h1>PHP-FPM</h1><br>
+      <img src="flop.gif"><br>
+      <h1>PHP-FPM</h1><br>';
---
+++ HW-6/public/www/index.php
@@ -0,0 +1,6 @@
+<?php
+echo '<h1>www pool works</h1>
+      <h2>Check out locations</h2>
+      <a href="docs/">docs</a>
+      <a href="img/">img</a>
+      <a href="video/">video</a>';
